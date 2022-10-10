b644791507450850900e9fe24ddba6b9db64dba5
vyacheslav1618 <normal301293@yandex.ru>
2022-10-10 17:44:19 +0500

    refactoring ArticlesPage.jsx

--- src/components/Articles/ArticlesPage.jsx
+++ src/components/Articles/ArticlesPage.jsx
@@ -11,24 +11,28 @@ const ArticleCategory = (props) => {
     </div>
 }
 
+const ArticlesListOfSelectedCategory = (props) => {
+    const path = "/articles/" + props.id
+    return <div className={s.category}>
+        <NavLink to={path}
+                        className={s.articlesListOfSelectedCategory}>{props.name}</NavLink>
+    </div>
+}
+
+const articleCategoryDataElements = articleCategoryData
+    .map(ac => <ArticleCategory name={ac.name} id={ac.id}/>);
+
+const articlesListOfSelectedCategoryDataElements = articlesListOfSelectedCategoryData
+    .map(al => <ArticlesListOfSelectedCategory name={al.name}/>)
+
 const ArticlesPage = () => {
 
     return <div className={s.articlePage}>
         <div className={s.articleCategories}>
-            <ArticleCategory name={articleCategoryData[0].name} id={articleCategoryData[0].id}/>
-            <ArticleCategory name={articleCategoryData[1].name} id={articleCategoryData[1].id}/>
-            <ArticleCategory name={articleCategoryData[2].name} id={articleCategoryData[2].id}/>
-            <ArticleCategory name={articleCategoryData[3].name} id={articleCategoryData[3].id}/>
-            <ArticleCategory name={articleCategoryData[4].name} id={articleCategoryData[4].id}/>
-            <ArticleCategory name={articleCategoryData[5].name} id={articleCategoryData[5].id}/>
+            {articleCategoryDataElements}
         </div>
         <div className={s.articlesListOfSelectedCategory}>
-            <NavLink to={articlesListOfSelectedCategoryData[0].id} className={s.articlesListOfSelectedCategory}>{articlesListOfSelectedCategoryData[0].name}</NavLink>
-            <NavLink to={articlesListOfSelectedCategoryData[1].id} className={s.articlesListOfSelectedCategory}>{articlesListOfSelectedCategoryData[1].name}</NavLink>
-            <NavLink to={articlesListOfSelectedCategoryData[2].id} className={s.articlesListOfSelectedCategory}>{articlesListOfSelectedCategoryData[2].name}</NavLink>
-            <NavLink to={articlesListOfSelectedCategoryData[3].id} className={s.articlesListOfSelectedCategory}>{articlesListOfSelectedCategoryData[3].name}</NavLink>
-            <NavLink to={articlesListOfSelectedCategoryData[4].id} className={s.articlesListOfSelectedCategory}>{articlesListOfSelectedCategoryData[4].name}</NavLink>
-            <NavLink to={articlesListOfSelectedCategoryData[5].id} className={s.articlesListOfSelectedCategory}>{articlesListOfSelectedCategoryData[5].name}</NavLink>
+            {articlesListOfSelectedCategoryDataElements}
         </div>
     </div>
 }
